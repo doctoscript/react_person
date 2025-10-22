0e0191cb7b1e372b54ba302ac15634554a012670
Pavlo Mikitjuk <mikitjukpavlo@gmail.com>
2025-10-22 22:03:39 +0300

    add solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,42 @@
-// export const Person = ({ person }) => ();
+import cn from 'classnames';
+
+export const Person = ({ person }) => {
+  const { name, age, sex, isMarried, partnerName } = person;
+
+  const isMale = sex === 'm';
+  const partnerRole = isMale ? 'wife' : 'husband';
+
+  return (
+    <section className="Person">
+      <h2
+        className={cn('Person__name', {
+          'Person__name--married': isMarried,
+          'Person__name--single': !isMarried,
+        })}
+      >
+        {`My name is ${name}`}
+      </h2>
+
+      {age && (
+        <p
+          className={cn('Person__age', {
+            'Person__age--adult': age >= 18,
+            'Person__age--minor': age < 18,
+          })}
+        >
+          {`I am ${age}`}
+        </p>
+      )}
+
+      <p
+        className={cn('Person__partner', {
+          'Person__partner--married': isMarried,
+          'Person__partner--single': !isMarried,
+        })}
+      >
+        {}
+        {isMarried ? `${partnerName} is my ${partnerRole}` : 'I am not married'}
+      </p>
+    </section>
+  );
+};
