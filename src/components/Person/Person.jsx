a3d59fa13040ef597793a79f8e1aa64044c57838
Pavlo Mikitjuk <mikitjukpavlo@gmail.com>
2025-10-27 20:38:06 +0200

    add simple solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -8,34 +8,11 @@ export const Person = ({ person }) => {
 
   return (
     <section className="Person">
-      <h2
-        className={cn('Person__name', {
-          'Person__name--married': isMarried,
-          'Person__name--single': !isMarried,
-        })}
-      >
-        {`My name is ${name}`}
-      </h2>
-      {age && (
-        <p
-          className={cn('Person__age', {
-            'Person__age--adult': age >= 18,
-            'Person__age--minor': age < 18,
-          })}
-        >
-          {`I am ${age}`}
-        </p>
-      )}
+      <h2 className={cn('Person__name', {})}>{`My name is ${name}`}</h2>
+      {age && <p className={cn('Person__age', {})}>{`I am ${age}`}</p>}
 
-      <p
-        className={cn('Person__partner', {
-          'Person__partner--married': isMarried,
-          'Person__partner--single': !isMarried,
-        })}
-      >
-        {isMarried
-          ? `${partnerName} is my ${partnerRole}`
-          : 'I am not married'}
+      <p className={cn('Person__partner', {})}>
+        {isMarried ? `${partnerName} is my ${partnerRole}` : 'I am not married'}
       </p>
     </section>
   );
